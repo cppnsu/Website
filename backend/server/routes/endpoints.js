37a243b9f2cc9a1d2a08ec601ded4a2649f2fe49
ChirayuRai <chirayurai@gmail.com>
2023-03-21 00:25:43 -0700

    testing out vercel

--- backend/server/routes/endpoints.js
+++ backend/server/routes/endpoints.js
@@ -11,7 +11,9 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
-
+data.route("/").get(function(req, res)) {
+  res.send("This is a test");
+}
 // This section will help you get a list of all the records.
 data.route("/:query").get(function(req, res) {
   let db_connect = dbo.getDb("dev-nsu-website");
