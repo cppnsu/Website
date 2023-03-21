fa5589ec03e852fb9b4c22069c14b953db7e7070
ChirayuRai <chirayurai@gmail.com>
2023-03-21 16:43:37 -0700

    connect to db THEN listen

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,12 +10,20 @@ app.use(require("./routes/endpoints"));
 // get driver connection
 const dbo = require("./db/conn");
 
+dbo.connectToServer(function(err) {
+  // connect to server, THEN LISTEN TO REQUESTS
+  if (err) console.error(err);
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+  })
+})
+
+/*
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function(err) {
     if (err) console.error(err);
-
   });
   console.log(`Server is running on port: ${port}`);
 });
-
+*/
