fd6a598b024d4e73b71e4c81984dc13939771ff5
ChirayuRai <chirayurai@gmail.com>
2023-03-21 20:24:26 -0700

    please

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: ".env" });
 const { MongoClient } = require('mongodb');
 const express = require('express');
 const app = express();
