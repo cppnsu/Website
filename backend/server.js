c36ee99aa232e5aa518d3404b7ff50e6683a95d8
ChirayuRai <chirayurai@gmail.com>
2023-03-21 22:25:21 -0700

    added comments

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,31 @@
-require("dotenv").config({ path: ".env" });
+// IMPORTS
 const { MongoClient } = require('mongodb');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000
+require("dotenv").config({ path: ".env" });
 
+// DECLARING CONSTANTS
+const PORT = process.env.PORT || 3000
 const uri = process.env.ATLAS_URI;
 const client = new MongoClient(uri);
 
+/* ROUTES */
 app.get("/:query", async (req, res) => {
-  let item = await client.db("dev-nsu-website")
+  let myQuery = await client.db("dev-nsu-website")
     .collection("Data")
-    .find({ Pages: req.params["query"] })
-    .toArray()
+    .find({ Pages: myQuery })
+    .toArray();
 
   return res.json(item)
 })
 
+
+// CONNECTION STRING
 client.connect(err => {
   if (err) { console.error(err); return false; }
   // connection to mongo is successful, listen for requests
   app.listen(PORT, () => {
-    console.log(`listening for requests on port ${PORT}`);
+    console.log("listening for requests");
   })
 });
 
