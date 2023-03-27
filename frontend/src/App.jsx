2bd0c6f94ba52d9cf7b7e698e0daca56993f4a0b
ChirayuRai <chirayurai@gmail.com>
2023-03-26 21:22:25 -0700

    added react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import useFetch from "./Hooks/useFetch";
 import "./index.css";
 import {
   Home,
